18d097797cedb2ffa07b2ea59c5b90b8fc4650ad
Jorge Sanz <jorsanzp@gmail.com>
2017-10-28 18:23:52 +0200

    refactor: Global refactor (#1)
    * docs(readme): Minor change
    
    * refactor(client): Rename variables
    
    * refactor(client): Remove debugging prints and fix internal doc
    
    * refactor(server): Rename variables
    
    * Minor refactor

--- daytime-udp-client-Sanz-Perez.c
+++ daytime-udp-client-Sanz-Perez.c
@@ -9,48 +9,48 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define BUFSIZE 1024
+
 /* wrapper for perror */
 void error(char *msg) {
     perror(msg);
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
 int main(int argc, char **argv)
 {
-    int s;
-    int port;
-    struct sockaddr_in server;
-    char message[32];
-    int n;
-    struct servent *appl_name;
-    unsigned int serverlen;
+    int s; /* socket */
+    int port; /* port number */
+    struct sockaddr_in server; /* server structure */
+    char buffer[BUFSIZE]; /* messages buffer */
+    int n; /* flag variable for errors */
+    struct servent *application_name; /* application structure */
+    unsigned int server_length; /* length of server structure */
 
     /* check if there is at least one program argument */
     if (argc < 2)
     {
-        fprintf(stderr, "usage: %s address.IP.server [port]\n", argv[0]);
-        exit(0);
+        fprintf(stderr, "usage: %s address.IP.server (mandatory) port-number (optional)\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
     /* get port number from arguments */
     if (argv[2])
     {
-        port = htons(atoi(argv[2])); /* Transform the port to network byte order */
-        printf("port assigned by program argument");
+        port = htons(atoi(argv[2]));
     }
     else
     { /* get the port by getservbyname when is not passed as argument */
-        appl_name = getservbyname("daytime", "udp");
+        application_name = getservbyname("daytime", "udp");
 
-        if (!appl_name)
+        if (!application_name)
         {
             printf("unknown application daytime\n");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
         else
         {
-            port = appl_name->s_port;
-            printf("getservbyname was successful\n");
+            port = application_name->s_port;
         }
     }
 
@@ -67,28 +67,25 @@ int main(int argc, char **argv)
     server.sin_port = port;                      /* Server Port        */
     server.sin_addr.s_addr = inet_addr(argv[1]); /* Server's Address   */
 
-    strcpy(message, "Hello");
+    /* set the message to be send */
+    strcpy(buffer, "Hello");
 
-    /* send the message in message to the server */
-    serverlen = sizeof(server);
-    printf("before sending... port 0x%x address 0x%x \n", server.sin_port, server.sin_addr.s_addr);
-    n = sendto(s, message, strlen(message), 0, (struct sockaddr *) &server, serverlen);
-    printf("after sending...\n");
+    /* send the message in buffer to the server */
+    server_length = sizeof(server);
+    n = sendto(s, buffer, strlen(buffer), 0, (struct sockaddr *) &server, server_length);
     if (n < 0)
         error("ERROR in sendto");
 
     /* receive the message from server */
-    printf("before receiving...\n");
     fflush(stdout);
-    serverlen = sizeof(server);
-    n = recvfrom(s, message, sizeof(message), 0, (struct sockaddr *) &server, &serverlen);
-    printf("after receiving...\n");
+    server_length = sizeof(server);
+    n = recvfrom(s, buffer, sizeof(buffer), 0, (struct sockaddr *) &server, &server_length);
     fflush(stdout);
     if (n < 0)
         error("ERROR in recvfrom");
 
-    /* print server reponse */
-    printf("Echo from server: %s", message);
+    /* print server response */
+    printf("%s", buffer);
     close(s);
     return 0;
 }
--- daytime-udp-server-Sanz-Perez.c
+++ daytime-udp-server-Sanz-Perez.c
@@ -9,7 +9,7 @@
 #include <errno.h>
 #include <unistd.h>
 
-#define BUFSIZE 32
+#define BUFSIZE 1024
 
 /* wrapper for perror */
 void error(char *msg)
@@ -22,19 +22,20 @@ int main(int argc, char **argv)
 {
 	int port; /* port to listen on */
 	int s;	/* socket */
-	int clientlen;
+	int client_length;
 	struct servent *appl_name;
-	struct sockaddr_in serveraddr;
-	struct sockaddr_in clientaddr;
-	char buffer[32];
-	int n;				/* message byte size */
+	struct sockaddr_in server_address;
+	struct sockaddr_in client_address;
+	char buffer[BUFSIZE];
+	int n; /* message byte size */
 	char hostname[128]; /* server hostname */
 	FILE *file;
+	char response[BUFSIZE];	
 
 	/* check arguments */
 	if (argc < 1)
 	{
-		fprintf(stderr, "usage: %s port\n", argv[0]);
+		fprintf(stderr, "usage: %s port (optional)\n", argv[0]);
 		exit(0);
 	}
 
@@ -56,7 +57,7 @@ int main(int argc, char **argv)
 		else
 		{
 			port = appl_name->s_port;
-			printf("getservbyname was successful\n");
+			printf("set port by servbyname()\n");
 		}
 	}
 
@@ -67,21 +68,21 @@ int main(int argc, char **argv)
 	if (s < 0)
 		error("ERROR opening socket");
 
-	serveraddr.sin_family = AF_INET;		 /* Server is in Internet Domain */
-	serveraddr.sin_port = port;				 /* Use any available port */
-	serveraddr.sin_addr.s_addr = INADDR_ANY; /* Server's Internet Address */
+	server_address.sin_family = AF_INET; /* server is in Internet Domain */
+	server_address.sin_port = port; /* use any available port */
+	server_address.sin_addr.s_addr = INADDR_ANY; /* server's Internet Address */
 
 	/* bind: associate the parent socket with a port */
-	if (bind(s, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+	if (bind(s, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
 		error("ERROR on binding");
 
-	/* main loop: wait for a datagram, then echo it */
-	clientlen = sizeof(clientaddr);
+	/* main loop: wait for a datagram and reply with server name and time */
+	client_length = sizeof(client_address);
 	while (1)
 	{
 		/* recvfrom: receive a UDP datagram from a client */
-		bzero(buffer, 32);
-		n = recvfrom(s, buffer, BUFSIZE, 0, (struct sockaddr *)&clientaddr, (socklen_t *)&clientlen);
+		bzero(buffer, BUFSIZE);
+		n = recvfrom(s, buffer, BUFSIZE, 0, (struct sockaddr *) &client_address, (socklen_t *) &client_length);
 		if (n < 0)
 			error("ERROR in recvfrom");
 
@@ -94,17 +95,23 @@ int main(int argc, char **argv)
 
 		/* get and print current daytime */
 		system("date > /tmp/tt.txt");
-		bzero(buffer, 32);
+		bzero(buffer, BUFSIZE);
 		file = fopen("/tmp/tt.txt", "r");
 		if (fgets(buffer, BUFSIZE, file) == NULL)
 		{
 			printf("Error in system(), in fopen(), or in fgets()\n");
 			exit(EXIT_FAILURE);
 		}
-		printf("Date: %s\n", buffer);
+		printf("Date: %s", buffer);
 
-		/* send date to client */
-		n = sendto(s, buffer, strlen(buffer), 0, (struct sockaddr *) &clientaddr, clientlen);
+		/* concat server name + date */
+		strcpy(response, hostname);
+		strcat(response, ": ");
+		strcat(response, buffer);
+		printf("%s\n", response);
+
+		/* send server name and date to client */
+		n = sendto(s, response, strlen(response), 0, (struct sockaddr *) &client_address, client_length);
 		if (n < 0)
 			error("ERROR in sendto");
 	}
